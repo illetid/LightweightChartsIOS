8d95fda605506f32e3f8884db37d6b335afe16a7
Andrii Ovcharenko <illetid@gmail.com>
2026-03-09 12:44:11 +0100

    Migrate Example app to UIScene Lifecycle

--- Example/LightweightCharts/AppDelegate.swift
+++ Example/LightweightCharts/AppDelegate.swift
@@ -3,16 +3,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        let window = UIWindow()
-        let viewController = TableViewController()
-        window.rootViewController = UINavigationController(rootViewController: viewController)
-        window.makeKeyAndVisible()
-        self.window = window
         return true
+    }
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
     }
 
 }
